d3a84c498130ab472d9af1ab48cfa8a540148d42
Sama Moayeri <smoy99@gmail.com>
2023-10-19 23:35:28 +0330

    second

--- main.js
+++ main.js
@@ -36,6 +36,18 @@ document.addEventListener('mousemove', (event) => {
     // Update light direction based on mouse position
     directionalLight.position.set(mouseX, mouseY, 1);
 });
+document.addEventListener('touchstart',(event) => {
+    const mouseX = (event.clientX / window.innerWidth) * 2 - 1;
+    const mouseY = -(event.clientY / window.innerHeight) * 2 + 1;
+
+    // Update light color based on mouse position
+    lightColor = new THREE.Color(mouseX + 1, mouseY + 1, 1);
+    directionalLight.color = lightColor;
+
+    // Update light direction based on mouse position
+    directionalLight.position.set(mouseX, mouseY, 1);
+});
+
 
 // Set camera position
 camera.position.z = 5;
